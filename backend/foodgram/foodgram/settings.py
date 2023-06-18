4cfd629358ad1a46920dd8039954bdb3a346d48e
Дмитрий Коржов <korzhovdima32@mail.ru>
2023-06-18 13:12:12 +0300

    № 3

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -29,14 +29,7 @@ SECRET_KEY = 'django-insecure-@h9m$xw&@-o9*-9+ly=uy8s*u4$coscqxa_75#=xp@y!ncy6df
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'backend',
-    'localhost',
-    '84.252.131.145',
-    'db',
-    'proxy',
-    'fronted']
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
